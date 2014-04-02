183aca40e6ddd9daf99d99049a317b00fb12863c
charles_yeh <charles_yeh@htc.com>
2014-04-02 15:04:22 +0800

    Prevent getKeyIdForName deadlock.
    Symptom: Deadlock.
    Root Cause: getKeyIdForName and database use different lock.
    
    Change-Id: If474fab2ababc45c4dc0d9a74a09de55cde75ab0

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -3127,8 +3127,13 @@ public class MediaProvider extends ContentProvider {
                 long artistRowId;
                 HashMap<String, Long> artistCache = helper.mArtistCache;
                 String path = values.getAsString(MediaStore.MediaColumns.DATA);
-                synchronized(artistCache) {
-                    Long temp = artistCache.get(s);
+
+                {
+                    Long temp = null;
+                    synchronized(artistCache) {
+                        temp = artistCache.get(s);
+                    }
+
                     if (temp == null) {
                         artistRowId = getKeyIdForName(helper, db,
                                 "artists", "artist_key", "artist",
@@ -3145,7 +3150,7 @@ public class MediaProvider extends ContentProvider {
                 values.remove("album");
                 long albumRowId;
                 HashMap<String, Long> albumCache = helper.mAlbumCache;
-                synchronized(albumCache) {
+                {
                     int albumhash = 0;
                     if (albumartist != null) {
                         albumhash = albumartist.hashCode();
@@ -3155,7 +3160,11 @@ public class MediaProvider extends ContentProvider {
                         albumhash = path.substring(0, path.lastIndexOf('/')).hashCode();
                     }
                     String cacheName = s + albumhash;
-                    Long temp = albumCache.get(cacheName);
+
+                    Long temp = null;
+                    synchronized(albumCache) {
+                        temp = albumCache.get(cacheName);
+                    }
                     if (temp == null) {
                         albumRowId = getKeyIdForName(helper, db,
                                 "albums", "album_key", "album",
@@ -4472,8 +4481,11 @@ public class MediaProvider extends ContentProvider {
                         if (artist != null) {
                             long artistRowId;
                             HashMap<String, Long> artistCache = helper.mArtistCache;
-                            synchronized(artistCache) {
-                                Long temp = artistCache.get(artist);
+                            {
+                                Long temp = null;
+                                synchronized(artistCache) {
+                                    temp = artistCache.get(artist);
+                                }
                                 if (temp == null) {
                                     artistRowId = getKeyIdForName(helper, db,
                                             "artists", "artist_key", "artist",
@@ -4528,9 +4540,13 @@ public class MediaProvider extends ContentProvider {
                             String s = so.toString();
                             long albumRowId;
                             HashMap<String, Long> albumCache = helper.mAlbumCache;
-                            synchronized(albumCache) {
+
+                            {
+                                Long temp = null;
                                 String cacheName = s + albumHash;
-                                Long temp = albumCache.get(cacheName);
+                                synchronized(albumCache) {
+                                    temp = albumCache.get(cacheName);
+                                }
                                 if (temp == null) {
                                     albumRowId = getKeyIdForName(helper, db,
                                             "albums", "album_key", "album",
@@ -5473,7 +5489,9 @@ public class MediaProvider extends ContentProvider {
         }
 
         if (cache != null && ! isUnknown) {
-            cache.put(cacheName, rowId);
+            synchronized (cache) {
+                cache.put(cacheName, rowId);
+            }
         }
         return rowId;
     }
